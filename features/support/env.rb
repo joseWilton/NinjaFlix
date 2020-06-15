3e192a6fcdf77b75dd1ad23567111de977704e06
Josewilton <jwilton@studiovisual.com.br>
2020-06-15 19:37:23 -0300

    iniciando o projeto

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+    config.app_host = "http://localhost:8080/"
+    config.default_max_wait_time = 15
+    
+end
