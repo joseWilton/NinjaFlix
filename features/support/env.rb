93c2d4da74094418eb25980837a317b48f303d76
Josewilton <jwilton@studiovisual.com.br>
2020-06-24 12:00:34 -0300

    realizando upload de imagens + ajustes no step de cadastro

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,11 @@
 require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+require 'os'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
     config.app_host = "http://localhost:8080"
-    config.default_max_wait_time = 15
+    config.default_max_wait_time = 5
     
 end
