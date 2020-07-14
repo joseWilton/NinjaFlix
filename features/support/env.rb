2bcf637c0ae920079ab904403b83ff9e58259352
Josewilton <jwilton@studiovisual.com.br>
2020-07-14 19:35:51 -0300

    estrutura para execucao no firefox, chrome e headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,8 +5,20 @@ require 'os'
 
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
+case ENV["BROWSER"]
+when "firefox"
+    @driver = :selenium
+when "chrome"
+    @driver = :selenium_chrome
+when "headless"
+    @driver = :selenium_chrome_headless
+else
+    puts "browser inválido"
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+
+    config.default_driver = @driver
     config.app_host = CONFIG["url"]
     config.default_max_wait_time = 5
     
