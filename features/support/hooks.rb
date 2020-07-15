5bc7622708a34040cccb57523ff8293bbccab90f
Josewilton <jwilton@studiovisual.com.br>
2020-07-15 15:52:53 -0300

    criacao do relatorio + ajustes finais do projeto

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,6 @@
+require 'report_builder'
+require 'date'
+
 Before do
     @login_page = LoginPage.new
     @user_page  = SidebarView.new
@@ -10,4 +13,25 @@ Before("@login") do
     user = CONFIG["users"]["cat_manager"]
     @login_page.go
     @login_page.with(user["email"],user["pass"])
+end
+
+After do
+
+    temp_shot = page.save_screenshot("log/temp_shot.png")
+    screenshot = Base64.encode64(File.open(temp_shot, "rb").read)
+    embed(screenshot, "image/png", "Screenshot")
+end
+
+d = DateTime.now
+@current_date = d.to_s.tr(":", "-")
+at_exit do
+
+    ReportBuilder.configure do |config|
+        config.input_path = 'log/report.json'
+        config.report_path = 'log/'+ @current_date
+        config.report_types = [:retry, :html]
+        config.report_title = 'NinjaFlix -WebApp'
+        config.additional_info = {"App" => "Web", "Data de execução" => @current_date}
+    end
+    ReportBuilder.build_report
 end
